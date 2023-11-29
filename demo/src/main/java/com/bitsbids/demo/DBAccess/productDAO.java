3f3d000ac2bd4e3430fb92bce313d46ad674616d
leHax27 <gursehaj27@gmail.com>
2023-11-30 04:58:06 +0530

    AWS Changed

--- demo/src/main/java/com/bitsbids/demo/DBAccess/productDAO.java
+++
@@ -1,8 +0,0 @@
-package com.bitsbids.demo.DBAccess;
-
-import com.bitsbids.demo.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface productDAO extends JpaRepository<Product, String> {
-    void deleteById(String id);
-}
